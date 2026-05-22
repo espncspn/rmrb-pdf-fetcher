bfd81195fe38152a54d76d0e1b75139d700a4cf5
espncspn <114963404+espncspn@users.noreply.github.com>
2026-05-22 11:51:58 +0800

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -38,7 +38,7 @@ def send_email():
     msg.attach(part)
 
     with smtplib.SMTP_SSL(smtp_host, smtp_port) as server:
-    server.login(sender, password)
+        server.login(sender, password)
         server.sendmail(sender, receiver, msg.as_string())
         print(f"发送成功！收件人：{receiver}")
 
