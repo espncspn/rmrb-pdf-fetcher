49574aa3a8ea49ee93752c20208c41faef73bd24
espncspn <114963404+espncspn@users.noreply.github.com>
2026-05-22 15:39:14 +0800

    Update rmrb_merged.py

--- rmrb_merged.py
+++ rmrb_merged.py
@@ -4,8 +4,6 @@ from urllib.parse import urljoin
 
 from datetime import datetime
 today = datetime.now().strftime("%Y%m/%d")
-from datetime import datetime
-today = datetime.now().strftime("%Y%m/%d")
 base_url = f"https://paper.people.com.cn/rmrb/pc/layout/{today}/node_01.html"
 root_url = "https://paper.people.com.cn/rmrb/"
 
